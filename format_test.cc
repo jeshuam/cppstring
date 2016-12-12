8d9ee56d29a45bdee0aa6a13d50055a15b18d037
Jeshua Morrissey <jeshuamorrissey@gmail.com>
2016-12-12 05:30:53 +0000

    Travis ci (#1)
    * added basic .travis.yaml
    
    * Rename .travis.yaml to .travis.yml
    
    * Update .travis.yml
    
    * Update .travis.yml
    
    * Update .travis.yml
    
    * Update .travis.yml
    
    * Update .travis.yml
    
    * Update .travis.yml
    
    * Update .travis.yml
    
    * Update .travis.yml
    
    * Update .travis.yml
    
    * Update .travis.yml
    
    * fixed format test
    
    * fixed format test

--- format_test.cc
+++ format_test.cc
@@ -94,15 +94,15 @@ TEST(TestFormatMap, TestFormatMapComplexObject) {
 }
 
 TEST(TestFormatMap, TestFormatMapShowLeftBrackets) {
-  ASSERT_STREQ("{{abc", string::FormatMap("{{abc", {}).c_str());
+  ASSERT_STREQ("{{abc", string::FormatMap("{{abc", {{"abc", 0}}).c_str());
 }
 
 TEST(TestFormatMap, TestFormatMapShowRightBrackets) {
-  ASSERT_STREQ("abc}}", string::FormatMap("abc}}", {}).c_str());
+  ASSERT_STREQ("abc}}", string::FormatMap("abc}}", {{"abc", 0}}).c_str());
 }
 
 TEST(TestFormatMap, TestFormatMapShowBothBrackets) {
-  ASSERT_STREQ("{{abc}}", string::FormatMap("{{abc}}", {}).c_str());
+  ASSERT_STREQ("{{abc}}", string::FormatMap("{{abc}}", {{"abc", 0}}).c_str());
 }
 
 TEST(TestFormatMap, TestFormatMapShowMixedBrackets) {
@@ -184,7 +184,7 @@ TEST(TestFormat, TestFormatWorksWhenSkippingTags) {
 /// Error Cases
 ///
 TEST(TestFormatMap, TestFormatMapWithMismatchedLeftBrackets) {
-  ASSERT_THROW(string::FormatMap("{", {}), std::invalid_argument);
+  ASSERT_THROW(string::FormatMap("{", {{"abc", "abc"}}), std::invalid_argument);
 }
 
 TEST(TestFormatMap, TestFormatMapWithMismatchedRightBrackets) {
@@ -193,7 +193,8 @@ TEST(TestFormatMap, TestFormatMapWithMismatchedRightBrackets) {
 }
 
 TEST(TestFormatMap, TestFormatMapWithInvalidTagContent) {
-  ASSERT_THROW(string::FormatMap("{ {}", {}), std::invalid_argument);
+  ASSERT_THROW(string::FormatMap("{ {}", {{"abc", "abc"}}),
+               std::invalid_argument);
 }
 
 TEST(TestFormatMap, TestFormatMapWithInvalidType) {
